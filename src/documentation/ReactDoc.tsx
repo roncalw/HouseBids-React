c4ef3cd7195e4dbf9f8414206f1c404249ef4956
roncalw <roncalw@hotmail.com>
2024-05-21 17:25:17 -0400

    Touched up a couple of pages for better formatting

--- src/documentation/ReactDoc.tsx
+++ src/documentation/ReactDoc.tsx
@@ -3,8 +3,12 @@ const ReactDoc = () => {
 return (
     <div className="row mb-2">
     <h5 className="themeFontColor text-center">
-      React Front End to MS ASP.NET Core middle tier to MS SQLite Backend
+      React Document<br/>
+      <span className="subNote">(Includes Documentation on APIs and Database)</span>
     </h5>
+    <h6 className="themeFontColor text-center">
+      Click <a href="/">here</a> to return back to the home page.<br/>
+    </h6>
     <iframe className="frameClass" id="pdf-js-viewer" src="https://www.codefest.com/cfpdfviewer/web/viewer.html?file=../pdf/ReactDoc.pdf" title="webviewer"></iframe>
 
   </div>
--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -8,19 +8,20 @@ type Args = {
 const Header = ({ subtitle }: Args) => {
   return (
     <header className="row mb-4">
-      <div className="col-5">
+      <div className="col-2">
       <a href='https://www.codefest.com'>
         <img src={logo} className="logo" alt="logo" />
       </a>
       </div>
-      <div className="col-7 mt-5">
+      <div className="col-10 mt-5">
         <span className="subtitle"> {subtitle}</span><br/> 
-        React app that shows a list of houses; click to edit or to add a bid.<br/>
+        Welcome to this example application!<br/>
+        This app was built using React for the frontend and ASP.NET Core for the backend APIs.<br/>
         Click 
         <Link to={`/reactdoc`}>
          here
         </Link>
-         for documentation!      
+         for documentation on how this app was built!      
       </div>
     </header>
   );
